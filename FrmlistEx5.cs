16d273e1473f6a671d4afa6712a99c28ed8553dc
YuNLT <yunilarthinn97@gmail.com>
2024-08-19 14:08:47 +0630

    Add files via upload

---
+++ FrmlistEx5.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace radioandcombo
+{
+    public partial class FrmlistEx5 : Form
+    {
+        public FrmlistEx5()
+        {
+            InitializeComponent();
+        }
+        double purchase;
+        double total;
+        double tax;
+        private void cboTax_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lstTax.Items.Clear();
+            purchase = int.Parse(txtPurchase.Text);
+            if (cboTax.Text == "tax exempt")
+            {
+                lstTax.Items.Add("0%");
+                tax = 0;
+            }
+            else if (cboTax.Text == "state sales tax only")
+            {
+                lstTax.Items.Add("3%");
+                tax = purchase * 0.03;
+            }
+            else if (cboTax.Text == "federal and state sales tax")
+            {
+                lstTax.Items.Add("5%");
+                tax = purchase * 0.05;
+            }
+            else 
+            {
+                lstTax.Items.Add("7%");
+                tax = purchase * 0.07;
+            }
+            total = purchase + tax;
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+           MessageBox.Show("total amount="+ total.ToString()+"\n sales tax"+tax.ToString(), "Purchase information",MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Frmlistex3.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace radioandcombo
+{
+    public partial class Frmlistex3 : Form
+    {
+        public Frmlistex3()
+        {
+            InitializeComponent();
+        }
+        private void cboMonth_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lstDays.Items.Clear();
+            if (int.Parse(txtYear.Text) % 4 == 0)
+            {
+                if (cboMonth.Text == "February")
+                {
+                    lstDays.Items.Add("29");
+                }
+                else if (cboMonth.Text == "April")
+                {
+                    lstDays.Items.Add("30");
+                }
+                else if (cboMonth.Text=="June")
+                {
+                    lstDays.Items.Add("30");
+                }
+                else if (cboMonth.Text == "September")
+                {
+                    lstDays.Items.Add("30");
+                }
+                else if (cboMonth.Text == "November")
+                {
+                    lstDays.Items.Add("30");
+                }
+                else
+                {
+                    lstDays.Items.Add("31");
+                }
+            }
+            else
+            {
+                if (cboMonth.Text == "February")
+                {
+                    lstDays.Items.Add("28");
+                }
+                else if (cboMonth.Text == "April")
+                {
+                    lstDays.Items.Add("30");
+                }
+                else if (cboMonth.Text == "June")
+                {
+                    lstDays.Items.Add("30");
+                }
+                else if (cboMonth.Text == "September")
+                {
+                    lstDays.Items.Add("30");
+                }
+                else if (cboMonth.Text == "November")
+                {
+                    lstDays.Items.Add("30");
+                }
+                else
+                {
+                    lstDays.Items.Add("31");
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtYear.Clear();
+            cboMonth.Select();
+            lstDays.ClearSelected();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
